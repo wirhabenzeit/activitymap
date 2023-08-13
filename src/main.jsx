ee8b95fc0ba69fa669281391e78c4c9feffdfbb1
Dominik Schröder <dschroeder@ethz.ch>
2023-08-13 23:39:09 +0200

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import { ActivityContextProvider } from "./contexts/ActivityContext.jsx";
 import { FilterContextProvider } from "./contexts/FilterContext.jsx";
 import { MapContextProvider } from "./contexts/MapContext.jsx";
 import { ListContextProvider } from "./contexts/ListContext.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { SelectionContextProvider } from "./contexts/SelectionContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router basename={import.meta.env.VITE_BASEPATH}>
+  <Router basename="/">
     <ActivityContextProvider>
       <MapContextProvider>
         <ListContextProvider>
