d22a97c5a61022b852b11441be18fcf014d66141
Dominik Schröder <dschroeder@ethz.ch>
2024-05-24 17:49:47 +0200

    Revert "promise fix"
    This reverts commit 7d6e343847b922d7839ee21a10fe0b4af9a0ca12.

--- src/contexts/Activity.ts
+++ src/contexts/Activity.ts
@@ -171,19 +171,18 @@ export const activitySlice: StateCreator<
         //return 0;
         const promises = await getActivitiesPaged({
           athlete_id: athleteId,
-          pageSize: 100,
+          pageSize: 200,
         });
-        const acts = (await Promise.all(promises)).flat();
-        /*for (const promise of promises) {
+        console.log("promises", promises);
+        for (const promise of promises) {
           try {
             const data = await promise;
             set(setActivities(data));
           } catch (e) {
             console.error(e);
           }
-        }*/
-        set(setActivities(acts));
-        return acts.length;
+        }
+        return promises.length;
       } else {
         const acts = await getDBActivities({ids});
         set(setActivities(acts));
